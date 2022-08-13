fba8f6a2d848ea49be188fe175fcdb2c5ac0c044
anna03121996 <101927808+anna03121996@users.noreply.github.com>
2022-08-13 07:41:41 +0530

    commit

---
+++ day_02sql_and_java_basic/java_basic/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+        String a = new String("anna");
+        
+        // try{
+            System.out.println(a);
+        // }
+        // catch{
+        //     Error
+        // }
+    }
+}
